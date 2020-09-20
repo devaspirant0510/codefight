52625fd9cb804f1364ae0e2fd6dcae9665b18704
Seung <68223593+devaspirant0510@users.noreply.github.com>
2020-09-20 23:28:49 +0900

    Update CodeSignal_add.py

--- intro/CodeSignal_add.py
+++ intro/CodeSignal_add.py
@@ -1,3 +1,10 @@
 #https://blog.naver.com/nova020510/222083443195
-def add(param1, param2):
+#=================================================
+#case1
+def add(param1, param2):#기본적인 함수
     return param1+param2
+
+#=================================================
+#case2
+def  add(a,b): # 람다 함수
+    return (lambda x, y: x + y)(a, b)
