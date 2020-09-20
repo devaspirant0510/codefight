7d105d4813ff4b0252c21d58193262e19798bfcc
Seung <68223593+devaspirant0510@users.noreply.github.com>
2020-09-20 23:21:02 +0900

    Update CodeSignal_centuryFromYear.py

--- intro/CodeSignal_centuryFromYear.py
+++ intro/CodeSignal_centuryFromYear.py
@@ -1,6 +1,6 @@
 #https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
 def centuryFromYear(year):
-    if year%100==0:
+    if year%100==0:#100으로 나누어 떨어질경우 몫만 구함
         return year//100
-    else:
+    else:#100으로 나누어 떨어지지 않을경우 몫에다 1 더함
         return year//100+1      
